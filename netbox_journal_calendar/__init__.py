521d7c7bd1663e9907d68d3e1177dfab6c3cc065
Stefano Paris <stefano.paris@gabecaspa.local>
2026-03-25 13:49:49 +0100

    Fix bootstrap 7

--- netbox_journal_calendar/__init__.py
+++ netbox_journal_calendar/__init__.py
@@ -5,7 +5,7 @@ class JournalCalendarConfig(PluginConfig): # Usa PluginConfig
     name = 'netbox_journal_calendar'
     verbose_name = 'Journal Calendar'
     description = 'Calendario interattivo per Journal Entries (Device, VM, Service)'
-    version = '1.5.1'
+    version = '1.5.2'
     base_url = 'journal-calendar'
 
 config = JournalCalendarConfig
