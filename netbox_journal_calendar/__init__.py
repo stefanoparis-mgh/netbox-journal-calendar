c5fc557c531ac1f4b4355d64414c70f7bb48ebdb
stefanoparis-mgh <stefano.paris@marcellogabana.it>
2026-03-26 10:52:11 +0100

    La versione

--- netbox_journal_calendar/__init__.py
+++ netbox_journal_calendar/__init__.py
@@ -5,7 +5,7 @@ class JournalCalendarConfig(PluginConfig): # Usa PluginConfig
     name = 'netbox_journal_calendar'
     verbose_name = 'Journal Calendar'
     description = 'Calendario interattivo per Journal Entries (Device, VM, Service)'
-    version = '2.0.0'
+    version = '2.0.1'
     base_url = 'journal-calendar'
 
 config = JournalCalendarConfig
