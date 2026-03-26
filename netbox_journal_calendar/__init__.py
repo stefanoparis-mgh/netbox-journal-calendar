076157ce191a09bb0c089a3a206f721244bf3749
stefanoparis-mgh <stefano.paris@marcellogabana.it>
2026-03-26 08:51:25 +0100

    Passaggio a tooltip

--- netbox_journal_calendar/__init__.py
+++ netbox_journal_calendar/__init__.py
@@ -5,7 +5,7 @@ class JournalCalendarConfig(PluginConfig): # Usa PluginConfig
     name = 'netbox_journal_calendar'
     verbose_name = 'Journal Calendar'
     description = 'Calendario interattivo per Journal Entries (Device, VM, Service)'
-    version = '1.5.6'
+    version = '1.5.7'
     base_url = 'journal-calendar'
 
 config = JournalCalendarConfig
