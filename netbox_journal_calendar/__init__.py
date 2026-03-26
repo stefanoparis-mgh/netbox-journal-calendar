2cce22e1f870dda55b5abc5eb9a725bf5efab7db
stefanoparis-mgh <stefano.paris@marcellogabana.it>
2026-03-26 08:54:17 +0100

    Rimosso bootstrap

--- netbox_journal_calendar/__init__.py
+++ netbox_journal_calendar/__init__.py
@@ -5,7 +5,7 @@ class JournalCalendarConfig(PluginConfig): # Usa PluginConfig
     name = 'netbox_journal_calendar'
     verbose_name = 'Journal Calendar'
     description = 'Calendario interattivo per Journal Entries (Device, VM, Service)'
-    version = '1.5.7'
+    version = '1.5.8'
     base_url = 'journal-calendar'
 
 config = JournalCalendarConfig
