91b41330be683d8ba0c391d5e1b9f6f8bc1efd5b
stefanoparis-mgh <stefano.paris@marcellogabana.it>
2026-05-14 14:54:23 +0200

    Fix filters (all)

--- netbox_journal_calendar/__init__.py
+++ netbox_journal_calendar/__init__.py
@@ -6,7 +6,7 @@ class JournalCalendarConfig(PluginConfig):
     name = 'netbox_journal_calendar'
     verbose_name = _('Journal Calendar')  # <--- Usa _() per la traduzione
     description = _('A visual calendar for Journal entries')
-    version = ('2.5.9')
+    version = ('2.6.0')
     author = 'Stefano Paris'
     base_url = 'journal-calendar'
     menu = 'navigation.menu'
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class BuildMessages(build):
 
 setup(
     name='netbox-journal-calendar',
-    version='2.5.9',
+    version='2.6.0',
     description='Interactive Calendar for Journal Entries',
     long_description='Display journal entry as a calendar view',
     url='https://github.com/stefanoparis-mgh/netbox-journal-calendar',
